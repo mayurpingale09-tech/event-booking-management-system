56319ce0e8d2aa2586058526b1a6cffbc79e3a61
mayurpingale9872 <mayurpingale2152@gmail.com>
2026-06-14 15:28:15 +0530

    Add login settings

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
