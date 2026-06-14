8aebceec14a2a6bdeaa545462fe5c7d8ace28871
mayurpingale9872 <mayurpingale2152@gmail.com>
2026-06-14 15:07:01 +0530

    Fix ALLOWED_HOSTS

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)!d77v&_60b-jmf6ln=^gl770eak19n5c0-kdfbpj@r-xni#m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
